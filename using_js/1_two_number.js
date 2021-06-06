768b4802ad7276c6e2b81f4ac908f46cb684011f
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-06-07 00:44:41 +0500

    Changed

--- using_js/1_two_number.js
+++ using_js/1_two_number.js
@@ -1,6 +1,6 @@
 let result = [];
-let two_numbers = function(nums, target){ for(let i = 1; i <= nums.length; i++){
-for(let j = 0; j <= nums.length; j++){
+let two_numbers = function(nums, target){ for(let i = 0; i <= nums.length; i++){
+for(let j = i+1; j <= nums.length; j++){
  if(nums[i]+nums[j]==target){
    return [i, j]; 
 }  }
