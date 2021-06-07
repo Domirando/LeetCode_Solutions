0315e208781b91d5c9a0c74d15874d4281130a10
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-06-07 15:06:44 +0500

    Changed

--- using_js/1_two_number.js
+++ using_js/1_two_number.js
@@ -1,9 +1,28 @@
-let result = [];
-let two_numbers = function(nums, target){ for(let i = 0; i <= nums.length; i++){
+// ------- 1-way --------
+let two_numbers_1 = function(nums, target){ 
+for(let i = 0; i <= nums.length; i++){
 for(let j = i+1; j <= nums.length; j++){
  if(nums[i]+nums[j]==target){
    return [i, j]; 
 }  }
 }
 }
-console.log(two_numbers([22, 7, 11, 2], 9))
+
+
+// -------- 2-way --------
+let twoSum = function(nums, target) {
+    let indices = {};
+  for(let i = 0; i <= nums.length; i++){
+    indices[nums[i]] = i;
+}
+  for(let i = 0; i<=nums.length; i++){
+    let r = target - nums[i];
+
+  if(indices[r] !== undefined && i !== indices[r]){
+    return [i, indices[r]]
+}
+} 
+}
+
+console.log(two_numbers_1([1, 2, 3, 4], 6));
+console.log(twoSum([1, 2, 3, 4], 6)); 
