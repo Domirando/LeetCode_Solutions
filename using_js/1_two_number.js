c4be425d347a0334d73bceea5234b4d8cedddc09
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-06-07 22:59:39 +0500

    Changed

--- using_js/1_two_number.js
+++ using_js/1_two_number.js
@@ -12,9 +12,9 @@ for(let j = i+1; j <= nums.length; j++){
 // -------- 2-way --------
 let twoSum = function(nums, target) {
     let indices = {};
-  for(let i = 0; i <= nums.length; i++){
-    indices[nums[i]] = i;
-}
+nums.forEach((item, index) => {
+    indices[item] = index;
+})
   for(let i = 0; i<=nums.length; i++){
     let r = target - nums[i];
 
@@ -24,5 +24,25 @@ let twoSum = function(nums, target) {
 } 
 }
 
+// --------- 3-way ----------
+let twoSum_2 = function(nums, target) {
+    let indices = new Map();
+  for(let i = 0; i<=nums.length; i++){
+    let r = target - nums[i];
+  if(indices.has(r)){
+    return [indices.get(r), i]
+}
+   indices.set(nums[i], i)
+} 
+} 
+
+
 console.log(two_numbers_1([1, 2, 3, 4], 6));
-console.log(twoSum([1, 2, 3, 4], 6)); 
+console.log(twoSum([1, 2, 3, 4], 7)); 
+console.log(twoSum_2([1, 2, 3, 4], 5)); 
+
+
+
+
+
+
---
+++ using_js/map.js
@@ -0,0 +1,5 @@
+console.log("Hello world!");
+let person1 = new Map();
+person1.set(firstName, "Marina");
+person1.set(lastName, "Mogilko");
+console.log(person1.get('firstName');
