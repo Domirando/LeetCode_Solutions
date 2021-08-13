d9f6ff1a7feddbd456ba25089d5e3548d65ca6d8
Vohidjonovna <maftunavohidjonovna@gmail.com>
2021-08-13 23:50:50 +0500

    Adding palindrome program

---
+++ using_js/palindrome.js
@@ -0,0 +1,15 @@
+var isPalindrome = function(x) {
+    var r, temp, final = 0;
+    temp = x;
+    while (x>0){
+        r = x % 10;
+        x = parseInt(x / 10);
+        final = final * 10 + r;
+    }
+    if(final == temp){
+        return true;
+    }else {
+        return false;
+    }
+    };
+console.log(isPalindrome (121));
