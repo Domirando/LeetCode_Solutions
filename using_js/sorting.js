da4db6c23245840c04dfe6f4f027925b23263051
Domirando <vohidjonovnamaftuna@gmail.com>
2025-12-19 13:16:26 +0500

    feat: sorting algorithm in js:

---
+++ using_js/sorting.js
@@ -0,0 +1,26 @@
+function smallest_fn(arr){
+    let min = 0;
+    for(let i=1; i<=arr.length-1; i++){
+        if (arr[min]>arr[i]){
+            min = i
+        }
+    }
+    return arr[min]
+}
+
+function sorting_fn(arr){
+    let len = arr.length
+    let sorted = []
+    let smallest = smallest_fn(arr)
+    while (sorted.length != len){
+        sorted.push(smallest)
+        arr = arr.filter( i => {
+            arr[i] != smallest
+            console.log(smallest+" "+arr[i])
+        })
+        smallest = smallest_fn(arr)
+    }
+    return sorted
+}
+
+console.log(sorting_fn([-1, -2, 1, 4, 8, 7, 3, 1, 2]))
