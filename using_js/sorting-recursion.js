028bf93f941c00e698d04818e25026544a6fd9d7
Domirando <vohidjonovnamaftuna@gmail.com>
2025-12-22 13:07:53 +0500

    Feat: sorting with recursion

---
+++ using_js/sorting-recursion.js
@@ -0,0 +1,22 @@
+function smallest_fn(arr){
+    let min = 0;
+    for(let i=1; i<=arr.length-1; i++){
+        if (arr[min]>arr[i]){
+            min = i
+        }
+    }
+    return min
+}
+
+function sorting_fn(arr){
+    let sorted = [];
+    while(!arr.is_empty){
+        let smallest = smallest_fn(arr)
+        sorted.push(arr[smallest])
+        arr = arr.filter(item => item!=arr[smallest])
+        sorting_fn(arr)
+    }
+    return sorted
+}
+
+console.log(sorting_fn([-1, -2, -3, 1, 4, 8, 9, 7, 3, 9, 8, 1, 2]))
