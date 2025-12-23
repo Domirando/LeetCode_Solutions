30a984c009108a6656060bacdd277c03f8a75778
Domirando <vohidjonovnamaftuna@gmail.com>
2025-12-23 21:37:29 +0500

    feat: sorting_fn added

--- using_rs/sorting-recursion.rs
+++ using_rs/sorting-recursion.rs
@@ -1,9 +1,20 @@
+macro_rules! sorting_fn {
+    ($arr: expr, $sorted: expr) => {
+        sorting_fn($arr, $sorted);
+    };
+
+    ($arr: expr) => {
+        sorting_fn($arr, []);
+    };
+}
+
 fn smallest_fn(arr: &[i64]) -> usize{
     return 0
 }
 
 fn sorting_fn(arr: &[i64], sorted: [i64] = []) -> [i64] {
 
+    return
 }
 
 fn main(){
