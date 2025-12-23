d8f73f2aa2c025941022426ab63b4cf1f4359197
Domirando <vohidjonovnamaftuna@gmail.com>
2025-12-23 21:50:50 +0500

    feat: sorting with recursion

--- using_rs/sorting-recursion.rs
+++ using_rs/sorting-recursion.rs
@@ -1,3 +1,12 @@
+fn smallest_fn(arr: &[i64]) -> usize{
+    return 0
+}
+
+fn sorting_fn(arr: &[i64], sorted: &[i64]) -> Vec<i64> {
+    let mut sorted = Vec::new();
+    sorted
+}
+
 // Macros are called with ! in the end
 macro_rules! sorting_fn {
     ($arr: expr, $sorted: expr) => {
@@ -5,19 +14,11 @@ macro_rules! sorting_fn {
     };
 
     ($arr: expr) => {
-        sorting_fn($arr, []);
+        sorting_fn($arr, &[]);
     };
-}
-
-fn smallest_fn(arr: &[i64]) -> usize{
-    return 0
-}
-
-fn sorting_fn(arr: &[i64], sorted: [i64] = []) -> [i64] {
-    [1]
 }
 
 fn main(){
     let arr = [-1, -2, -3, 1, 4, 8, 9, 7, 3, 9, 8, 1, 2];
-    println!("{}", sorting_fn(&arr));
+    println!("{:?}", sorting_fn!(&arr));
 }
